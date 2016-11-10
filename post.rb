5e0e605bda2fe8915e9d3ce446a8d9942b6edbdd
AlexZ188 <az@79element.com>
2016-11-10 22:25:45 +0800

    Notepad v.1, working

--- post.rb
+++ post.rb
@@ -1,5 +1,13 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
@@ -14,15 +22,26 @@ class Post
   end
 
   def save
+    file = File.new(file_path, "w:UTF-8") # открываем файл на запись
+
+    for item in to_strings do # идем по массиву строк, полученных из метода to_strings
+      file.puts(item)
+    end
+
+    file.close # закрываем
+  end
+
+'''
+  def save
     file = File.new(file_path, "w:UTF-8")
 
     for item in to_strings do
-      fileputs(item)
+      file.puts(item)
     end
 
     file.close
   end
-
+'''
   def file_path
     current_path = File.dirname(__FILE__)
 
